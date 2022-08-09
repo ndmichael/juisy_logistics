781d0af6ddd75161797908370b5b4a0083cc5d04
ndmichael <ukejemicheal@gmail.com>
2022-08-09 04:16:33 +0100

    changed debug to true

--- logistic/settings.py
+++ logistic/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY_SDELIVERY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.environ.get("DEBUG_VALUE") == "True"
+DEBUG = True
+# DEBUG = os.environ.get("DEBUG_VALUE") == "True"
 
 ALLOWED_HOSTS = ['targeco.herokuapp.com', 'https://targeco.co', 'localhost', '127.0.0.1']
 
