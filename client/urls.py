210fb3717419f365f75eb6d4c7bcdcee3ab4dca5
ndmichael <ukejemicheal@gmail.com>
2023-09-25 13:50:10 +0100

    routes for all_shipments page

--- client/urls.py
+++ client/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import dashboard, create_shipment
+from .views import dashboard, create_shipment, shipments
 
 
 
 urlpatterns = [
     path('create-shipment/', create_shipment, name="create-shipment" ),
+    path('shipments/', shipments, name="shipments" ),
     path('<str:username>/', dashboard, name="client-dashboard" ),
 ]
